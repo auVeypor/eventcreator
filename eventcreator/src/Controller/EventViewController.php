d12d852dd96b9e151e0c380d2f0716464c0ca04e
auVeypor <lm671@uowmail.edu.au>
2016-08-03 23:59:55 -0400

    Commented the controller for displaying the links to separate events

--- eventcreator/src/Controller/EventViewController.php
+++ eventcreator/src/Controller/EventViewController.php
@@ -3,13 +3,21 @@
 namespace Drupal\eventcreator\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+
 
 class EventViewController extends ControllerBase {
 	public function content($aid, $vid) {
+		// This is needed so we can construct the URL
 		global $base_url;
+			
+		// Load the nodes (events) from the IDs passed from the URL
+		$att = Node::load($aid);
+		$vol = Node::load($vid);
 
-		$links = "<a href=" . $base_url . "/node/" . $aid . ">Attendee Event</a><br>";
-		$links .= "<a href=" . $base_url . "/node/" . $vid . ">Volunteer Event</a>";
+		// Construct the HTML to display (two links corresponding to each event)
+		$links .= "<a href=" . $base_url . "/node/" . $aid . ">" . $att->label() . "</a><br>";
+		$links .= "<a href=" . $base_url . "/node/" . $vid . ">" . $vol->label() . "</a>";
 		$build = array(
 			'#type' => 'markup',
 			'#markup' => $this->t($links)
